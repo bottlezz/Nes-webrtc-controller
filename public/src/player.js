40b763034a3658965ef3524abf4dd66cb73010bf
Alan Wong <alwo@microsoft.com>
2019-07-22 12:48:22 -0700

    add player.js

--- public/src/player.js
+++ public/src/player.js
@@ -1,4 +1,5 @@
-(function () {
+function Player () {
+
     var lastPeerId = null;
     var peer = null; // own peer object
     var conn = null;
@@ -36,7 +37,67 @@
             alert('' + err);
         });
     };
-
+    /**
+     * Create the connection between the two Peers.
+     *
+     * Sets up callbacks that handle any events related to the
+     * connection and data received on it.
+     */
+    function join(connectionId) {
+        // Close old connection
+        if (conn) {
+            conn.close();
+        }
+        // Create connection to destination peer specified in the input field
+        conn = peer.connect(connectionId, {
+            reliable: true
+        });
+        conn.on('open', function () {
+            // status.innerHTML = "Connected to: " + conn.peer;
+            console.log("Connected to: " + conn.peer);
+            // Check URL params for comamnds that should be sent immediately
+            var command = getUrlParam("command");
+            if (command)
+                conn.send(command);
+        });
+        // Handle incoming data (messages only since this is the signal sender)
+        conn.on('data', function (data) {
+            // addMessage("<span class=\"peerMsg\">Peer:</span> " + data);
+        });
+        conn.on('close', function () {
+            // status.innerHTML = "Connection closed";
+        });
+    };
+    /**
+     * Get first "GET style" parameter from href.
+     * This enables delivering an initial command upon page load.
+     *
+     * Would have been easier to use location.hash.
+     */
+    function getUrlParam(name) {
+        name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+        var regexS = "[\\?&]" + name + "=([^&#]*)";
+        var regex = new RegExp(regexS);
+        var results = regex.exec(window.location.href);
+        if (results == null)
+            return null;
+        else
+            return results[1];
+    };
+    /**
+     * Send a signal via the peer connection and add it to the log.
+     * This will only occur if the connection is still alive.
+     */
+    function signal(sigName) {
+        if (conn.open) {
+            conn.send(sigName);
+            console.log(sigName + " signal sent");
+        }
+    }
     
-    
-});
+    return {
+        init: initialize,
+        join: join,
+        sendCommand: signal
+    }
+};
